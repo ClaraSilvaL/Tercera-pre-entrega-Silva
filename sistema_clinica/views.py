d1f05c9fa5fd5216691debe963898de822c57604
Clara Silva <clara.silvaleon1@gmail.com>
2023-05-09 21:44:52 -0500

    Agregada template base y vista que consume template

--- sistema_clinica/views.py
+++ sistema_clinica/views.py
@@ -7,3 +7,12 @@ def saludar(request):
     saludo = "Bienvenido al sistema"
     pagina_html = HttpResponse(saludo)
     return pagina_html
+
+def saludar_con_html(request):
+    contexto = {}
+    http_responde = render(
+        request=request,
+        template_name='control_clinica/base.html',
+        context=contexto,
+    )
+    return http_responde
