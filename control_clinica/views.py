a095710eab7d6bf0242f17aa3781cc2a2c0a82e2
Clara Silva <clara.silvaleon1@gmail.com>
2023-05-11 12:58:12 -0500

    Cambios a las views para leer pacientes y diagnosticos de la base de datos

--- control_clinica/views.py
+++ control_clinica/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
+from control_clinica.models import *
+
 # Create your views here.
 
 def listar_pacientes(request):
     contexto = {
-        "pacientes": [
-            {"nombre": "Eva", "apellido": "Garcia"},
-            {"nombre": "Valeria", "apellido": "Rojas"},
-        ]
+        "pacientes": Paciente.objects.all(),
     }
     http_response = render(
         request=request,
@@ -18,10 +17,7 @@ def listar_pacientes(request):
 
 def listar_diagnosticos(request):
     contexto = {
-        "diagnosticos": [
-            {"enfermedad": "Gripe", "estado": "leve"},
-            {"enfermedad": "Gripe", "estado": "moderado"},
-        ]
+        "diagnosticos": Diagnostico.objects.all(),
     }
     http_response = render(
         request=request,
