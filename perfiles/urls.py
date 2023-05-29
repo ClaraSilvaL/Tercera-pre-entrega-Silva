cb83f35b1d6a6d1ece92a4ab462b7f5bbcf8f1b4
Clara Silva <clara.silvaleon1@gmail.com>
2023-05-28 20:18:35 -0500

    Agregue login, logout y mejoras a la navegacion

--- perfiles/urls.py
+++ perfiles/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from perfiles.views import registro
+from perfiles.views import registro, login_view, CustomLogoutView
 
 urlpatterns = [
     # URLS Usuario y sesion
-   path('registro/', registro, name="registro"),
-
+    path('registro/', registro, name="registro"),
+    path('login/', login_view, name="login"),
+    path('logout/', CustomLogoutView.as_view(), name="logout"),
 ]
--- perfiles/views.py
+++ perfiles/views.py
@@ -3,6 +3,11 @@ from django.urls import reverse, reverse_lazy
 
 from perfiles.forms import UserRegisterForm
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
+
 # Create your views here.
 
 def registro(request):
@@ -20,3 +25,28 @@ def registro(request):
        template_name='perfiles/registro.html',
        context={'form': formulario},
    )
+
+def login_view(request):
+   if request.method == "POST":
+       form = AuthenticationForm(request, data=request.POST)
+
+       if form.is_valid():
+           data = form.cleaned_data
+           usuario = data.get('username')
+           password = data.get('password')
+           user = authenticate(username=usuario, password=password)
+           # user puede ser un usuario o None
+           if user:
+               login(request=request, user=user)
+               url_exitosa = reverse('inicio')
+               return redirect(url_exitosa)
+   else:  # GET
+       form = AuthenticationForm()
+   return render(
+       request=request,
+       template_name='perfiles/login.html',
+       context={'form': form},
+   )
+
+class CustomLogoutView(LogoutView):
+   template_name = 'perfiles/logout.html'
